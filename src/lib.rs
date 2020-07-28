be29eebc156c7ba2043f0e3cdcbb67e18f8ad190
Alexander Serebryakov <alex.serebr@gmail.com>
2020-07-28 13:57:52 +0300

    Adds basic statistics printing out

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,31 @@
 mod config;
 mod generator;
+mod statistics;
 
 use config::Config;
 use config::IpList;
 use std::io::prelude::*;
-use log::{info, debug};
+use log::{info, debug, trace};
 extern crate log;
 use std::net::TcpStream;
 use std::str::FromStr;
 use generator::RandomRequestGenerator;
+use statistics::Statistics;
 
-fn send_request(request: &str, address: &str) {
+
+fn send_request(request: &str, address: &str, stats: &mut Statistics) {
+    let buffer_size = 1024;
     let mut stream = TcpStream::connect(address).unwrap();
-    let mut response = vec![0;256];
+    let mut response = vec![0;buffer_size];
 
     stream.write(&request.as_bytes()).unwrap();
     stream.read(response.as_mut_slice()).unwrap();
 
     let response_string = String::from_utf8(response).unwrap();
 
-    match response_string.find("403 Forbidden") {
-        Some(_) => debug!("FORBIDDEN"),
-        None => debug!("Response:\n{}\n", response_string),
-    }
+    stats.count_response(response_string.lines().next().unwrap().to_string());
+    debug!("Response : {}", response_string.lines().next().unwrap().to_string());
+    trace!("First {} bytest of response:\n{}\n", buffer_size, response_string);
 }
 
 pub fn run(config_path: &str) -> Result<(), std::io::Error> {
@@ -34,7 +37,9 @@ pub fn run(config_path: &str) -> Result<(), std::io::Error> {
         ips: IpList::from_str(config.network.as_str()).unwrap(),
     };
 
+    let mut stats = Statistics::new();
     loop {
-        send_request(generator.generate_request().as_str(), config.target.as_str());
+        send_request(generator.generate_request().as_str(), config.target.as_str(), &mut stats);
+        print!("Statistics {}\n", stats);
     }
 }
